5943729edab44c202e93c6f7738fb8d1a9c94846
samhithamuvva <163280630+samhithamuvva@users.noreply.github.com>
2024-12-15 00:13:23 -0800

    Update settings.py

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -25,7 +25,7 @@ ML_SERVICE_URL = "http://diseasify-ml.eastus.azurecontainer.io:8080"
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
