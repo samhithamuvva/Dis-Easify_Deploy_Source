121fedf2078b5112358752d04c79a8c765e1ceac
samhithamuvva <163280630+samhithamuvva@users.noreply.github.com>
2024-12-15 16:00:09 -0800

    Update urls.py

--- mini_app1/urls.py
+++ mini_app1/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('diabetes', views.diabetes, name='diabetes'),
-    path('heart_disease', views.heart_disesase, name='heart_disease'),
+    path('heart_disease', views.heart_disease, name='heart_disease'),
     path('diabetes_result', views.diabetes_result, name='diabetes_result'),
     path('heart_disease_result', views.heart_disease_result, name='heart_disease_result'),
     path('breast_cancer', views.breast_cancer, name='breast_cancer'),
